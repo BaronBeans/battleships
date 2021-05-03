f26496473afb12b29fdca3b0420b5426d1242fac
Sean Barnard <sean.barnard.94@hotmail.com>
2021-05-03 12:15:24 +0100

    .

--- packages/client/src/core/store/game.store.ts
+++ packages/client/src/core/store/game.store.ts
@@ -1,3 +1,16 @@
 export interface GameState {
   inProgress: boolean;
+  gameState: {};
+  currentTurn: "p" | "o";
 }
+
+export interface Game {
+  player: Player;
+  opponent: Player;
+  turns: number;
+  time: number;
+}
+
+export interface Player {}
+
+export interface BoardStatus {}
